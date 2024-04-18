79d3813df0f6301fe0e64fca57a2596bc210fe07
JEFIRO <150871603+JEFIRO@users.noreply.github.com>
2024-04-18 15:18:26 -0300

    frames iniciais

--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from tkinter import *
-
-
-class Biblioteca:
-    def __init__(self):
-        self.janela = Tk()
-        self.janela.title("Biblioteca")
-        self.janela.geometry('1300x700')
+class MinhaApp:
+    def __init__(self, janela):
+        self.janela = janela
+        self.preto = 'black'
+        self.frame_login()
+        self.frame_aluno()
+        self.frame_butons()
         self.janela.mainloop()
 
-    def frame(self):
+    def frame_login(self):
+        self.frame_tv = Frame(self.janela, bg=self.preto, width=900, height=550)
+        self.frame_tv.grid(column=1, row=0, padx=0, pady=0, sticky="nsew")  # Posicionando à direita
 
+    def frame_aluno(self):
+        self.frame_tv_aluno = Frame(self.janela, bg='red', width=460, height=550)
+        self.frame_tv_aluno.grid(column=0, row=0, padx=0, pady=0, sticky="nsew")  # Posicionando à esquerda
 
+    def frame_butons(self):
+        self.frame_butao = Frame(self.janela, bg='blue', width=90, height=218)
+        self.frame_butao.grid(column=0, row=1, padx=0, pady=0, columnspan=2, sticky="nsew")  # Posicionando abaixo, estendendo por duas colunas
 
-main = Biblioteca()
+if __name__ == "__main__":
+    root = Tk()
+    app = MinhaApp(root)
